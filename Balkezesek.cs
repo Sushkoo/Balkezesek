aa8a58be1fc20e8a9f9f455acfb66e19a36c9b0c
Sushi <70635877+Sushkoo@users.noreply.github.com>
2023-11-14 12:20:30 +0100

    Add files via upload

---
+++ Balkezesek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Balkezesek
+    {
+        public string nev;
+        public DateTime elso;
+        public DateTime utolso;
+        public double suly;
+        public double magassag;
+    
+
+    public Balkezesek(string sor)
+    {
+        string[] tordeltSor = sor.Split(";");
+        string[] elsoTordeltDatum = tordeltSor[1].Split("-");
+        string[] utolsoTordeltDatum = tordeltSor[2].Split("-");
+
+        nev = tordeltSor[0];
+        elso = new DateTime(Convert.ToInt32(elsoTordeltDatum[0]), Convert.ToInt32(elsoTordeltDatum[1]), Convert.ToInt32(elsoTordeltDatum[2]));
+        utolso = new DateTime(Convert.ToInt32(utolsoTordeltDatum[0]), Convert.ToInt32(utolsoTordeltDatum[1]), Convert.ToInt32(utolsoTordeltDatum[2]));
+        suly = Convert.ToDouble(tordeltSor[3]);
+        magassag = Convert.ToDouble(tordeltSor[4]);
+    }
+}
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ConsoleApp8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+   
+    string[] sorok = File.ReadAllLines("C:\\Users\\szilagyi.dominik\\source\\repos\\ConsoleApp8\\ConsoleApp8\\bin\\Debug\\net6.0\\balkezesek.csv");
+
+    List<Balkezesek> jatekosok = new List<Balkezesek>();
+
+    for(int i = 1; i<sorok.Length; i++)
+        {
+            Balkezesek Balkezesek = new Balkezesek(sorok[i]);
+            jatekosok.Add(Balkezesek);
+        }
+
+        Console.WriteLine("3. feladat:");
+        jatekosok.Count();
+
+        Console.WriteLine("4. feladat:");
+
+    }
+}
